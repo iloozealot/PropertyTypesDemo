2f5a4e2dc4347cc0b3ead4e4ea6a06645dc25fcd
iloozealot <iloozealot@gmail.com>
2023-11-19 16:18:53 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using ConsoleUI;
+
+PersonModel person = new PersonModel();
+
+person.FirstName = "Justin";
+//person.LastName = "Spencer";
+person.SSN = "123-45-6789";
+
+Console.WriteLine(person.FullName);
+Console.WriteLine(person.SSN);
+
+Console.ReadLine();
